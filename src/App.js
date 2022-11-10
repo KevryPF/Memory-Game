a66188d3a84138b8bc7f8eff7653bc0771c32bb6
Kevry <kevry.paulino06@myhunter.cuny.edu>
2022-11-10 15:08:28 -0500

    card shuffle functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import Card from './components/cards.js'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  let arr =[] //Array to keep track of current pair
+function App(props) {
+  let cardVals = ["Card1","Card2","Card3","Card4","Card1","Card2","Card3","Card4"]
+  const [cardV, setCardv] = useState([])
+  
+  useEffect(() =>{
+    setCardv(["Card1","Card2","Card3","Card4","Card1","Card2","Card3","Card4"])
+  }, [])
+
+  let arr = [] //Array to keep track of current pair
+  let paired = [] //Array to keep track of paired cards
 
   function handleChoice(card){
       arr.push(card)
@@ -11,6 +20,8 @@ function App() {
           if(arr[0].props.cardText == arr[1].props.cardText){ //Pair is a match
             console.log("MATCH!")
             handlePause()
+            paired.push(arr[0])
+            paired.push(arr[1])
             arr[0].handlePaired()
             arr[1].handlePaired()
             arr = []
@@ -29,7 +40,8 @@ function App() {
     setTimeout(() => document.getElementById("gameContainer").style.pointerEvents = 'auto', 1000)
   }
 
-  function shuffle(cards){
+  function shuffle(cards){  //Shuffles Cards and resets selected cards
+    console.log("Shuffle")
     let numofCards = cards.length, temp, randomNum
     while(numofCards) {
       randomNum = Math.floor(Math.random() * (numofCards)--)
@@ -37,15 +49,27 @@ function App() {
       cards[numofCards] = cards[randomNum]
       cards[randomNum] = temp
     }
-    return cards
+    for (let i = 0; i < paired.length; i++){
+      paired[i].handlePaired()
+      paired[i].handleReset()
+    }
+    setCardv(cards)
   }
-
-  let cardVals = ["Card1","Card2","Card3","Card4","Card1","Card2","Card3","Card4"]
-  shuffle(cardVals);
+  
   return (
-    <div className="App" id="gameContainer">
-      {cardVals.map((item,idx)=>{return <Card key={idx} disabled={false} cardText={item} isFlipped={true} handleChoice={handleChoice}/>})}
+    <>
+    
+    <div className='content'>
+      <div className='buttonDiv'>
+        <button className="shuffle" onClick = {() => shuffle(cardVals)}>Shuffle</button>
+      </div>
+      <div className="App" id="gameContainer">
+        {cardV.map((item,idx)=>{return <Card key={idx} disabled={false} cardText={item} handleChoice={handleChoice}/>})}
+      </div>
     </div>
+
+    </>
+
   );
 }
 
