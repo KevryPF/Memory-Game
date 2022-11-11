2ff3ebb0d5bbf05331e2e69a921aba8542b48ea3
Kevry <kevry.paulino06@myhunter.cuny.edu>
2022-11-11 13:56:24 -0500

    Added win functionality

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import Card from './components/cards.js'
 import React, { useEffect, useState } from 'react';
 import { render } from '@testing-library/react';
+import Popup from './components/popup';
 
 function App(props) {
   let cardVals = ["Card1","Card2","Card3","Card4","Card1","Card2","Card3","Card4"]
   const [cardV, setCardv] = useState([])
+  const [paired, setPaired] = useState([])
+  const [wins, setWins] = useState()
   
   useEffect(() =>{
     setCardv(["Card1","Card2","Card3","Card4","Card1","Card2","Card3","Card4"])
+    setPaired([])
+    setWins(0)
   }, [])
 
   let arr = [] //Array to keep track of current pair
-  let paired = [] //Array to keep track of paired cards
+  let paired1 = [] //Array to keep track of paired cards
 
   function handleChoice(card){
       arr.push(card)
@@ -20,11 +25,15 @@ function App(props) {
           if(arr[0].props.cardText == arr[1].props.cardText){ //Pair is a match
             console.log("MATCH!")
             handlePause()
-            paired.push(arr[0])
-            paired.push(arr[1])
+            setPaired(paired.concat(arr))
             arr[0].handlePaired()
             arr[1].handlePaired()
             arr = []
+            if (paired.length == cardVals.length-2){  //Win Case
+              console.log("WIN")
+              setWins(wins+1)
+              document.getElementsByClassName('Popup')[0].style["display"] = 'block';
+            }
           }
           if(arr[0].props.cardText != arr[1].props.cardText){ //Pair is not a match
             console.log("WRONG!")
@@ -33,6 +42,12 @@ function App(props) {
             arr[1].handleReset()
             arr = []
           }}
+  }
+
+
+  function handleWin() {
+    
+    shuffle(cardVals)
   }
 
   function handlePause(){ //Turns off click functionality when comparing card choices
@@ -53,18 +68,25 @@ function App(props) {
       paired[i].handlePaired()
       paired[i].handleReset()
     }
+    setPaired([])
     setCardv(cards)
   }
-  
+
   return (
     <>
     
     <div className='content'>
+      <Popup func = {() => shuffle(cardVals)}></Popup>
       <div className='buttonDiv'>
         <button className="shuffle" onClick = {() => shuffle(cardVals)}>Shuffle</button>
       </div>
       <div className="App" id="gameContainer">
         {cardV.map((item,idx)=>{return <Card key={idx} disabled={false} cardText={item} handleChoice={handleChoice}/>})}
+      </div>
+      <div>
+        <h1>Game Info</h1>
+        <p>Pairs matched: {paired.length/2}/{cardVals.length/2}</p>
+        <p>Times Won: {wins}</p>
       </div>
     </div>
 
